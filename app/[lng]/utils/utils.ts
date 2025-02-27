3f5f377eb2d3dfb285430db0fbff77cbc3d48378
Nick Carney <nickcarney@Nicks-MacBook-Air.local>
2025-02-27 15:47:31 -0700

    utils from story

---
+++ app/[lng]/utils/utils.ts
@@ -0,0 +1,15 @@
+import { Hex, http } from "viem";
+import { Account, privateKeyToAccount, Address } from "viem/accounts";
+import { StoryClient, StoryConfig } from "@story-protocol/core-sdk";
+
+const privateKey: Address = `0x${process.env.WALLET_PRIVATE_KEY}`;
+const account: Account = privateKeyToAccount(privateKey as Hex);
+export const SPGNFTContractAddress: Address =
+    (process.env.SPG_NFT_CONTRACT_ADDRESS as Address) || '0xc32A8a0FF3beDDDa58393d022aF433e78739FAbc'
+
+const config: StoryConfig = {
+  account: account,
+  transport: http(process.env.RPC_PROVIDER_URL),
+  chainId: "aeneid",
+};
+export const client = StoryClient.newClient(config);
