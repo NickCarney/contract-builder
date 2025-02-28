ab5036ac4192136f76beb60871bbb7977899c042
Nick Carney <nickcarney@Nicks-MacBook-Air.local>
2025-02-28 13:41:37 -0700

    api route to get png from pdf for nft

--- app/[lng]/api/convertPdf/route.ts
+++ app/[lng]/api/convertPdf/route.ts
@@ -1,15 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 import { fromPath } from "pdf2pic";
 import path from "path";
-import fs from "fs/promises";
 import { v4 as uuidv4 } from "uuid";
 import sharp from "sharp";
+import fsPromises from "fs/promises";
+
 
 async function mergeImages(imagePaths: string[], outputPath: string) {
     const images = await Promise.all(imagePaths.map((path) => sharp(path).toBuffer()));
     const { width, height } = await sharp(imagePaths[0]).metadata();
-  
-    sharp({
+    await sharp({
       create: {
         width: width!,
         height: height! * imagePaths.length,
@@ -20,11 +20,17 @@ async function mergeImages(imagePaths: string[], outputPath: string) {
     })
     .composite(images.map((img, i) => ({ input: img, top: i * 1000, left: 0 })))
     .toFile(outputPath);
+
+    console.log(outputPath);
+
+    return outputPath;
+    
   }
 
 export async function POST(req: NextRequest) {
   try {
     const { pdfUrl } = await req.json();
+    console.log(pdfUrl)
     if (!pdfUrl) {
       return NextResponse.json({ error: "Missing PDF URL" }, { status: 400 });
     }
@@ -39,14 +45,14 @@ export async function POST(req: NextRequest) {
       throw new Error(`Failed to fetch PDF: ${response.statusText}`);
     }
     const buffer = await response.arrayBuffer();
-    await fs.writeFile(tempPdfPath, Buffer.from(buffer));
+    await fsPromises.writeFile(tempPdfPath, Buffer.from(buffer));
 
     // Ensure file exists before converting
-    await fs.access(tempPdfPath);
+    await fsPromises.access(tempPdfPath);
 
     // Output directory
     const outputDir = path.join(process.cwd(), "public", "images");
-    await fs.mkdir(outputDir, { recursive: true });
+    await fsPromises.mkdir(outputDir, { recursive: true });
 
     // Convert PDF to Image
     const converter = fromPath(tempPdfPath, {
@@ -60,12 +66,14 @@ export async function POST(req: NextRequest) {
     const results = await converter.bulk(-1);
 
     const imagePaths = results.map(result => result.path!);
-    await mergeImages(imagePaths, path.join(outputDir, "combined.png"));
-
+    console.log(imagePaths);
+    const combinedPath = path.join(outputDir, "combined.png");
+    const outPath = await mergeImages(imagePaths, combinedPath);
+    console.log("DONE");
     // Cleanup
-    await fs.unlink(tempPdfPath);
+    await fsPromises.unlink(tempPdfPath);
 
-    return NextResponse.json({ success: true, imagePath: outputDir+'combined.png' });
+    return NextResponse.json({ success: true, imagePath: outPath });
   } catch (error) {
     console.error("Error processing IPFS PDF:", error);
     return NextResponse.json({ error: "Failed to convert PDF" }, { status: 500 });
