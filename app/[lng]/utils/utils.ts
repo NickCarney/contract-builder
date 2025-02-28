f19cff1cf2f2bab35f48ed0e2869007d55fea82c
Nick Carney <nickcarney@Nicks-MacBook-Air.local>
2025-02-28 16:08:06 -0700

    address as Address for vercel

--- app/[lng]/utils/utils.ts
+++ app/[lng]/utils/utils.ts
@@ -2,7 +2,7 @@ import { Hex, http } from "viem";
 import { Account, privateKeyToAccount, Address } from "viem/accounts";
 import { StoryClient, StoryConfig } from "@story-protocol/core-sdk";
 
-const privateKey: Address = `0x${process.env.WALLET_PRIVATE_KEY}`;
+const privateKey: Address = `0x${process.env.WALLET_PRIVATE_KEY}` as Address;
 const account: Account = privateKeyToAccount(privateKey as Hex);
 export const SPGNFTContractAddress: Address =
     (process.env.SPG_NFT_CONTRACT_ADDRESS as Address) || '0xc32A8a0FF3beDDDa58393d022aF433e78739FAbc'
